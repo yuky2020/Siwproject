b8c8183713368353a9edcdd7fda8c3d19d57aa7f
Matteo <yuky2020@gmai.com>
2019-06-06 22:21:17 +0200

    migliorato il design

--- src/main/java/siwproject/siwproject/controller/MainController.java
+++ src/main/java/siwproject/siwproject/controller/MainController.java
@@ -10,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import siwproject.siwproject.model.Album;
+import siwproject.siwproject.model.Foto;
 import siwproject.siwproject.model.Fotografo;
+import siwproject.siwproject.pg.AlbumService;
+import siwproject.siwproject.pg.FotoService;
 import siwproject.siwproject.pg.FotografoServices;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class MainController {
     @Autowired
     FotografoServices fotografoServices;
+    @Autowired
+    AlbumService albumServices;
+    @Autowired
+    FotoService fotoServices;
 
     @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
     public String index(Model model) {
@@ -39,6 +46,22 @@ public class MainController {
     @RequestMapping(value = { "/error" }, method = RequestMethod.GET)
     public String error(Model model) {
         return "404";
+    }
+
+    @RequestMapping(value = { "/mostraAlbum" }, method = RequestMethod.GET)
+    public String viewAlbum(Model model, HttpServletRequest req) {
+        List<Album> albums = albumServices.getAlbum();
+        req.setAttribute("albums", albums);
+
+        return "mostraAlbum";
+    }
+
+    @RequestMapping(value = { "/mostraFoto" }, method = RequestMethod.GET)
+    public String viewFoto(Model model, HttpServletRequest req) {
+        List<Foto> fotos = fotoServices.getAllFoto();
+        req.setAttribute("fotos", fotos);
+
+        return "mostraFoto";
     }
 
     @RequestMapping(value = { "/mostraFotografi" }, method = RequestMethod.GET)
--- src/main/java/siwproject/siwproject/pg/AlbumService.java
+++ src/main/java/siwproject/siwproject/pg/AlbumService.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import siwproject.siwproject.model.Album;
-
+@Service
 public class AlbumService   {
     @Autowired
     private AlbumRepository rep;
