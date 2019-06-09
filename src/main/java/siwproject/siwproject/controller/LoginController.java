6c9257e54bfa2e163787ea3ee9e82776802e0910
Matteo <yuky2020@gmai.com>
2019-06-09 13:58:09 +0200

    login mapp

--- src/main/java/siwproject/siwproject/controller/LoginController.java
+++ src/main/java/siwproject/siwproject/controller/LoginController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,6 +16,13 @@ import siwproject.siwproject.pg.AmministratoreServices;
 public class LoginController {
     @Autowired
     AmministratoreServices amministratoreServices;
+    
+    @GetMapping("/login")
+    public String paginaLogin(Model model) {
+        
+        return "login";
+    }
+    
 
     @PostMapping("/loginValidator")
     public String acessoAdmin(@RequestBody String username, @RequestBody String password, HttpServletRequest req) {
